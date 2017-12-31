d48746f6aa625e1daa762f01ad81197eaca70fd4
Reza Amuzadeh <amuzadeh@vasl.ir>
2017-12-31 15:27:29 +0330

    update package

---
+++ app/src/main/java/com/orpheusdroid/screenrecorder/viva/VivaActivity.java
@@ -0,0 +1,13 @@
+package com.orpheusdroid.screenrecorder.viva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VivaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viva);
+    }
+}
